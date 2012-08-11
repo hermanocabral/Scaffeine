012f4727b4a347ea36d0cb9ef58e75ae4bde19cd
Rod <rodmjay@wcpro.com>
2012-08-11 10:40:40 -0600

    initial commit

---
+++ src/Scaffeine/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Scaffeine")]
+[assembly: AssemblyDescription("Scaffeine Operating System")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("World-Class Programmers")]
+[assembly: AssemblyProduct("ScaffR")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("Scaffeine, ScaffR")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5b586874-8d3e-4137-9ba8-e982413416d5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
