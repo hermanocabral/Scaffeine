71c6f171abbc055584a4573134b8b381bc20b9cb
Rod <rodmjay@wcpro.com>
2012-08-23 14:06:33 -0600

    Added inbox functionality

---
+++ src/Scaffeine.Inbox/content/Controllers/InboxController.cs.pp
@@ -0,0 +1,13 @@
+﻿namespace MvcApplication31.Controllers
+{
+    using System.Web.Mvc;
+
+    public class InboxController : Controller
+    {
+        public ActionResult Index()
+        {            
+            return View();
+        }
+
+    }
+}
