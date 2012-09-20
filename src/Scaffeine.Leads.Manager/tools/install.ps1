dc05202182a0dffcfb135c5ea38b8b657d113d10
Rod Johnson <rod@scaffr.com>
2012-09-20 15:15:58 -0600

    Fixed bug where models weren't being scaffolded

--- src/Scaffeine.Leads.Manager/tools/install.ps1
+++ src/Scaffeine.Leads.Manager/tools/install.ps1
@@ -1,3 +1,8 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
 scaffold scaffr.frontend.for Lead
+scaffold scaffr.frontend.for Product
+scaffold scaffr.frontend.for Order
+scaffold scaffr.frontend.for Customer
+scaffold scaffr.frontend.for Lead
+scaffold scaffr.frontend.for OrderLineItem
--- src/Scaffeine.Leads.Services/tools/install.ps1
+++ src/Scaffeine.Leads.Services/tools/install.ps1
@@ -5,6 +5,10 @@ scaffold scaffeine.leads.services
 build-project
 
 scaffold scaffr.backend.for Lead
+scaffold scaffr.backend.for Product
+scaffold scaffr.backend.for Order
+scaffold scaffr.backend.for Customer
+scaffold scaffr.backend.for OrderLineItem
 
 add-migration "Leads Added" -projectName $dataProjectName
 
