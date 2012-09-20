87de04533f596eee70a9b5b7abc66e44c68c6d02
Rod Johnson <rod@scaffr.com>
2012-09-20 12:48:39 -0600

    Moved FileSystem Photo provider to photos project

--- src/Scaffeine.Photos.FileSystem/tools/install.ps1
+++ src/Scaffeine.Photos.FileSystem/tools/install.ps1
@@ -1,7 +1,3 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-add-project $photosFileSystemProjectName | with-reference "System.Configuration,System.Runtime.Serialization,System.Web,System.Drawing,$photosProjectName"
-
-get-project | with-reference $photosFileSystemProjectName
-
 scaffold Scaffeine.Photos.FileSystem
