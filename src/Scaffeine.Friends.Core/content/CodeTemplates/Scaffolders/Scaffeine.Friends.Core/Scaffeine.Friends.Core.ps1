3ac60d5e716fa06f95e8f5322ce81c8bede75510
Rafael dos Santos <p.4ever@gmail.com>
2012-09-22 10:18:13 -0300

    Friends package

---
+++ src/Scaffeine.Friends.Core/content/CodeTemplates/Scaffolders/Scaffeine.Friends.Core/Scaffeine.Friends.Core.ps1
@@ -0,0 +1,13 @@
+﻿[T4Scaffolding.Scaffolder()][CmdletBinding()]
+param(        
+    [string]$Project,
+	[string]$CodeLanguage,
+	[string[]]$TemplateFolders,
+	[switch]$Force = $false
+)
+
+Add-Template $coreProjectName "Model\Friends\InvitationStatus" "InvitationStatus" -Force:$Force $TemplateFolders
+Add-Template $coreProjectName "Model\Friends\CreateFriendshipStatus" "CreateFriendshipStatus" -Force:$Force $TemplateFolders
+
+Add-Domain "Model\Friends\FriendshipInvitation" "FriendshipInvitation" -Force:$Force $TemplateFolders
+Add-Domain "Model\Friends\Friendship" "Friendship" -Force:$Force $TemplateFolders
--- src/Scaffeine.Friends.Core/content/CodeTemplates/Scaffolders/Scaffeine.Friends.Core/readme.txt
+++
@@ -1,7 +0,0 @@
-﻿ps1 file
-
-all codefirst classes 
-
-filename.cs.t4
-
-look at scaffeine.leads.core, scaffeine.membership.core
