77ccb49878137b3ed6d40b54f71b611836e966f7
Rafael dos Santos <p.4ever@gmail.com>
2012-10-01 14:08:09 -0300

    updates

--- src/Scaffeine.Membership.Register/tools/install.ps1
+++ src/Scaffeine.Membership.Register/tools/install.ps1
@@ -1,2 +1,4 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
+Write-Host Registering service RegisterModel to autoMapper -ForegroundColor DarkGreen
+Add-CodeToMethod $baseProject.Name "\App_Start\" "AutoMapperConfig.cs" "AutoMapperConfig" "RegisterMappings" "AutoMapper.Mapper.CreateMap<RegisterModel, User>();"
