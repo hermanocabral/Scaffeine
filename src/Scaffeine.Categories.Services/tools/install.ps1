c2f1e83c7dc498d2aac5dc0ad414083de15d1b08
Rod Johnson <rod@scaffr.com>
2012-10-01 06:22:53 -0600

    various updates

---
+++ src/Scaffeine.Categories.Services/tools/install.ps1
@@ -0,0 +1,7 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+scaffold scaffr.backend.for Category
+
+#add-migration "Caregories Added" -projectName $dataProjectName
+
+#update-database -projectName $dataProjectName
