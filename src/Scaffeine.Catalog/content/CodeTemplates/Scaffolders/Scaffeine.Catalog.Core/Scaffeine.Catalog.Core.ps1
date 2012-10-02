5439ed9a27854b6b4d013a27b9038e141f402e94
Rod Johnson <rod@scaffr.com>
2012-10-01 23:22:35 -0600

    Fixed catalog data model and scaffolders

--- src/Scaffeine.Catalog/content/CodeTemplates/Scaffolders/Scaffeine.Catalog.Core/Scaffeine.Catalog.Core.ps1
+++ src/Scaffeine.Catalog/content/CodeTemplates/Scaffolders/Scaffeine.Catalog.Core/Scaffeine.Catalog.Core.ps1
@@ -6,7 +6,7 @@ param(
 	[switch]$Force = $false
 )
 
-Add-Template $coreProjectName "Model\Product.Catalog" "Product.Category" -Force:$Force $TemplateFolders
+Add-Template $coreProjectName "Model\Product.Catalog" "Product.Catalog" -Force:$Force $TemplateFolders
 Add-Template $coreProjectName "Model\Category.Catalog" "Category.Catalog" -Force:$Force $TemplateFolders
 Add-Template $coreProjectName "Model\Category.Product" "Category.Product" -Force:$Force $TemplateFolders
 Add-Domain "Model\CategoryFeaturedProduct" "CategoryFeaturedProduct" -Force:$Force $TemplateFolders
