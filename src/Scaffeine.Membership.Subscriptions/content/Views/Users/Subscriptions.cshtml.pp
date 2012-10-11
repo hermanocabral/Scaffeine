a392cd79df7155939c5878ca5837d750b67daa5c
Rod Johnson <rod@scaffr.com>
2012-10-11 11:38:52 -0600

    user sitemap stuff

--- src/Scaffeine.Membership.Subscriptions/content/Views/Users/Subscriptions.cshtml.pp
+++ src/Scaffeine.Membership.Subscriptions/content/Views/Users/Subscriptions.cshtml.pp
@@ -1,4 +1,4 @@
-﻿@{
+@{
     ViewBag.Title = "User Details";
     Layout = "~/Views/Shared/_Folder.cshtml";
 }
@@ -6,12 +6,9 @@
 @using $rootnamespace$.Extensions
 @model $rootnamespace$.Models.Users.UserViewModel
 
-<h2>@Model.FullName</h2>
+<h2>John Doe</h2>
 
 
 @Html.MvcSiteMap("Modules").Tabs(2,true,false,1)
-<div class="tab-content">
-    <div class="tab-pane active" id="details">
-        @Html.DisplayForModel("Bootstrap/Bootstrap.Table")
-    </div>
-</div>
+
+asdf
